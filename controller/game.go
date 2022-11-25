35f5e6abce0d61d01f36babc1df095223dd97481
Serveresty <cs.go.12228@gmail.com>
2022-11-26 00:08:46 +0300

    transactions and edit balance

--- controller/game.go
+++ controller/game.go
@@ -64,7 +64,13 @@ func (s *DataBase) GetDiceData(rw http.ResponseWriter, r *http.Request, p httpro
 
 	//Проверка диапазона умножения
 	if k, _ := strconv.ParseFloat(data.Multiply, 64); k <= 9500 && k >= 1.0106 {
-		send_data.Multiply = data.Multiply
+		rng, _ := strconv.ParseFloat(data.Range, 64)
+		wch, _ := strconv.ParseFloat(data.WinChance, 64)
+		if k == 95/rng && k == 95/wch {
+			send_data.Multiply = data.Multiply
+		} else {
+			send_data.OutOfMultiply = true
+		}
 	} else {
 		send_data.OutOfMultiply = true
 	}
@@ -93,6 +99,8 @@ func (s *DataBase) GetDiceData(rw http.ResponseWriter, r *http.Request, p httpro
 		send_data.Number2 = rand.Intn(9 + 1)
 		send_data.Number3 = rand.Intn(9 + 1)
 		send_data.Number4 = rand.Intn(9 + 1)
+		num := fmt.Sprintf("%d%d.%d%d", send_data.Number1, send_data.Number2, send_data.Number3, send_data.Number4)
+		s.transactionDice(num, send_data.Range, send_data.Profit, send_data.BetAmount, n)
 	} else {
 		send_data.Number1 = 0
 		send_data.Number2 = 0
@@ -102,4 +110,53 @@ func (s *DataBase) GetDiceData(rw http.ResponseWriter, r *http.Request, p httpro
 	rw.Header().Set("Content-Type", "application/json")
 	_ = json.NewEncoder(rw).Encode(send_data)
 	return
+}
+
+func (s *DataBase) transactionDice(n string, rng string, pt string, bt string, bal float64) map[string]any {
+	numb, _ := strconv.ParseFloat(n, 64)
+	rang, _ := strconv.ParseFloat(rng, 64)
+	prof, _ := strconv.ParseFloat(pt, 64)
+	bet, _ := strconv.ParseFloat(bt, 64)
+	var status string
+	var summ string
+
+	if rang > numb {
+		bal += prof
+		status = "win"
+		summ = "+" + fmt.Sprint(prof)
+	} else {
+		bal -= bet
+		status = "lost"
+		summ = "-" + fmt.Sprint(bet)
+	}
+
+	perem := `
+			INSERT INTO transactions (user_id, type, stat, summ) VALUES(?, ?, ?, ?)
+		`
+
+	//Добавляю в БД запись о регистрации, если нет ошибок
+	insert, errdb := s.Data.Query(perem, conditionsMap["username"], "DiceBet", status, summ)
+	defer func() {
+		if insert != nil {
+		}
+	}()
+	if errdb != nil {
+	}
+
+	conditionsMap["balance"] = fmt.Sprint(bal)
+
+	perem2 := `
+			UPDATE users_account SET balance = ? WHERE username = ?;
+		`
+
+	insert2, errdb2 := s.Data.Query(perem2, conditionsMap["balance"], conditionsMap["username"])
+	defer func() {
+		if insert2 != nil {
+		}
+	}()
+	if errdb2 != nil {
+	}
+
+	fmt.Println(conditionsMap["balance"])
+	return conditionsMap
 }
