d0ab73fb009c1489070e2dd264824a480aa6fdf7
Serveresty <cs.go.12228@gmail.com>
2022-11-30 18:03:49 +0300

    start to create chat

--- main.go
+++ main.go
@@ -8,8 +8,60 @@ import (
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/sessions"
+	"github.com/gorilla/websocket"
 	"github.com/julienschmidt/httprouter"
 )
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+type ConnectUser struct {
+	Websocket *websocket.Conn
+	ClientIP  string
+}
+
+func newConnectUser(ws *websocket.Conn, clientIP string) *ConnectUser {
+	return &ConnectUser{
+		Websocket: ws,
+		ClientIP:  clientIP,
+	}
+}
+
+var users = make(map[ConnectUser]int)
+
+func WebsocketHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	ws, _ := upgrader.Upgrade(w, r, nil)
+
+	defer func() {
+		if err := ws.Close(); err != nil {
+			log.Println("Websocket could not be closed", err.Error())
+		}
+	}()
+
+	log.Println("Client connected:", ws.RemoteAddr().String())
+	var socketClient *ConnectUser = newConnectUser(ws, ws.RemoteAddr().String())
+	users[*socketClient] = 0
+	log.Println("Number client connected ...", len(users))
+
+	for {
+		messageType, message, err := ws.ReadMessage()
+		if err != nil {
+			log.Println("Ws disconnect waiting", err.Error())
+			delete(users, *socketClient)
+			log.Println("Number of client still connected ...", len(users))
+			return
+		}
+
+		for client := range users {
+			if err = client.Websocket.WriteMessage(messageType, message); err != nil {
+				log.Println("Cloud not send Message to ", client.ClientIP, err.Error())
+			}
+		}
+
+	}
+}
 
 var encryptionKey = "something-very-secret"
 var loggedUserSession = sessions.NewCookieStore([]byte(encryptionKey))
@@ -55,6 +107,7 @@ func routes(r *httprouter.Router, DB *sql.DB) {
 	r.POST("/main-sign-up", handler.SignUp)
 	r.POST("/main-sign-in", handler.SignIn)
 	//
+	r.GET("/ws", WebsocketHandler)
 
 	r.GET("/", handler.HomePage)
 	r.GET("/logout", handler.LogoutHandler)
